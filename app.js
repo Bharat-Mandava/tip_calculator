eac96cf6addfc78d791cb0c29c14dec682a9e45e
Bharat-Mandava <43178562+Bharat-Mandava@users.noreply.github.com>
2019-06-23 11:56:34 -0400

    fixed decimal to 2

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ let serviceQuality= document.getElementById("serviceQuality").value
 let splitQuantity = document.getElementById("splitQuantity").value;
 if (splitQuantity>1){
 let totalbillwithtip= ((totalBill*serviceQuality)/splitQuantity)
-
+let roundedtotal=totalbillwithtip.toFixed(2)
  
    console.log(totalbillwithtip)
-   document.getElementById("resulttip").innerHTML =totalbillwithtip ;
-    return (totalbillwithtip)
+   document.getElementById("resulttip").innerHTML =roundedtotal ;
+    return (roundedtotal)
     
 } else {
     let totalbillwithtip =  (totalBill*serviceQuality)
